ea1587d825a1864bb81d61668575e21c169e8a37
root <root@DESKTOP-JAODQIH.localdomain>
2022-09-01 09:47:43 -0300

    Att00

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gissao-m <gissao-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 12:22:19 by gissao-m          #+#    #+#             */
+/*   Updated: 2022/08/26 15:44:57 by gissao-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+// int main(int argc, char const *argv[], char **envp)
+// {
+//     char **matriz;
+    
+//     matriz = malloc(sizeof(char*) * 3);
+//     matriz[0] = "ls";
+//     matriz[1] = "-l";
+//     matriz[2] = NULL;
+
+//     execve("/usr/bin/ls",matriz , envp);
+//     return 0;
+// }
